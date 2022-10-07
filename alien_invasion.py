5d30880264d25d3b6d4d7f3d9617c98dbf9f1e66
emmanuel michailidis <emmanuel.michailidis@outlook.com>
2022-10-07 13:21:20 +0300

    first attempt

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.clock=pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
@@ -14,6 +15,7 @@ class AlienInvasion:
                     sys.exit()
 # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
